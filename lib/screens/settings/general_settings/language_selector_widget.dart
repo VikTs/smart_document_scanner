0d288f6316c289c040d4afe09368f22239a60e0d
Viktoriia Tsukan <40521085+VikTs@users.noreply.github.com>
2026-06-08 05:25:17 +0300

    feat: updated design of language selector

--- lib/screens/settings/general_settings/language_selector_widget.dart
+++ lib/screens/settings/general_settings/language_selector_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
@@ -12,26 +11,40 @@ class LanguageSelector extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: EdgeInsets.zero,
-      title: Text("settings.language".tr()),
-      trailing: DropdownButton<Locale>(
-        value: currentLocale,
-        items: const [
-          DropdownMenuItem(
-            value: Locale('en'),
-            child: Text('English'),
-          ),
-          DropdownMenuItem(
-            value: Locale('uk'),
-            child: Text('Українська'),
-          ),
-        ],
-        onChanged: (locale) {
-          if (locale != null) {
-            context.setLocale(locale);
-          }
-        },
+    return Card(
+      elevation: 0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(
+                "settings.language".tr(),
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+
+            DropdownButton<Locale>(
+              value: currentLocale,
+              underline: const SizedBox(),
+              items: const [
+                DropdownMenuItem(
+                  value: Locale('en'),
+                  child: Text('English'),
+                ),
+                DropdownMenuItem(
+                  value: Locale('uk'),
+                  child: Text('Українська'),
+                ),
+              ],
+              onChanged: (locale) {
+                if (locale != null) {
+                  context.setLocale(locale);
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
